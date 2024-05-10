3ff0b606257f68405196d1ad8aad845631cb33eb
prashantbohra01 <prashantb4157@gmail.com>
2024-05-10 15:57:16 +0530

    react

---
+++ day-01/src/index.js
@@ -0,0 +1,10 @@
+// var React = require("react");
+import React from "react";
+
+// var ReactDOM = require("react-dom");
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+ <h1>Hello world</h1>,
+  document.getElementById("root")
+);
--- test/app.js
+++
@@ -1,5 +0,0 @@
-
-    const heading = React.createElement("h1", {id:"heading", class:"head"}, "Hello Coders");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-
-        root.render(heading);
