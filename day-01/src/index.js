3d4e754ecb6a527587ae5a40b2c7f1573ffc3a09
prashantbohra01 <prashantb4157@gmail.com>
2024-05-10 16:38:57 +0530

    Day-01

--- day-01/src/index.js
+++ day-01/src/index.js
@@ -4,7 +4,25 @@ import React from "react";
 // var ReactDOM = require("react-dom");
 import ReactDOM from "react-dom";
 
+
+let myName = "Prashant Bohra" 
+let curDate = new Date().toLocaleDateString();
+let curTime = new Date().toLocaleTimeString();
 ReactDOM.render(
- <h1>Hello world</h1>,
+  <>
+ <h1>Hello Guys</h1>
+ <p>yo</p>
+ <ol>
+  <li>Stranger things</li>
+  <li>Bleach</li>
+  <li>Money Heist</li>
+ </ol>
+
+<h2>Hello, my name is {myName}.</h2>
+<p>My lucky number is {Math.random()}. </p> 
+
+<p>Current date is = {curDate}</p>
+<p>Current time is = {curTime}</p>
+ </>,
   document.getElementById("root")
 );
