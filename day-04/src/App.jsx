d4efe145dc858db4b1908df7339200f5eadfe6b4
prashantbohra01 <prashantb4157@gmail.com>
2024-05-13 17:53:57 +0530

    Day-04 Props

--- day-04/src/App.jsx
+++ day-04/src/App.jsx
@@ -1,10 +1,38 @@
-import add from "./com"
+import Card from "./components/Card.jsx"
 
 function App() {
   return (
     <>
-        
-  
+      <Card imgsrc="https://wallpapercave.com/w/wp4056410.jpg"
+      title="A Netflix Original series"
+      sname="DARK"
+      link="https://www.netflix.com/in/title/80100172"
+      />
+
+      <Card 
+      imgsrc="https://wallpapercave.com/w/wp10339821.jpg"
+      title="A Netflix Original series"
+      sname="STRANGER THINGS"
+      link="https://www.netflix.com/in/title/80057281"
+      />
+      <Card 
+      imgsrc="https://wallpapercave.com/w/wp1882405.jpg"
+      title="A Netflix Original series"
+      sname="Jessica Jones"
+      link="https://www.netflix.com/in/title/80002311"
+      />
+      <Card 
+      imgsrc="https://wallpapercave.com/w/wp5852734.jpg"
+      title="A Netflix Original series"
+      sname="THE WITCHER"
+      link="https://www.netflix.com/in/title/80189685"
+      />
+      <Card 
+      imgsrc="https://wallpapercave.com/w/wp11321539.jpg"
+      title="A Netflix Original series"
+      sname="MONEY HEIST"
+      link="https://www.netflix.com/in/title/80192098"
+      />
     </>
   )
 }
---
+++ day-04/src/components/Card.jsx
@@ -0,0 +1,18 @@
+function Card(props){
+    return(
+    <div className="cards">
+        <div className="card">
+            <img src={props.imgsrc} alt="mypic" className="card-img"/>
+        <div className="card_info">
+            <span className="card_category">{props.title}</span>
+            <h3 className="card_title">{props.sname}</h3>
+            <a href={props.link} target="">
+                <button>Watch Now</button>
+            </a>
+        </div>
+        </div>
+    </div>
+    )
+}
+ 
+export default Card;
